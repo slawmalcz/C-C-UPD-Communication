118d329003c32376d01fc28f882c77576c000c66
Animx11 <animx11@gmail.com>
2018-05-15 14:20:01 +0200

    Rozdzielenie na funkcje aplikacji C

--- Server/CServer.c
+++ Server/CServer.c
@@ -5,45 +5,11 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(void)
-{
-	int SERVER_PORT = 25566;
-	
-	struct sockaddr_in server_addres;
-	memset(&server_addres, 0, sizeof(server_addres));
-	server_addres.sin_family = AF_INET;
-	
-	server_addres.sin_port = htons(SERVER_PORT);
-	
-	server_addres.sin_addr.s_addr = htonl(INADDR_ANY);
-	
-	int listen_sock;
-	
-	
-	if((listen_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0){
-		printf("Could not create listen socket\n");
-		return 1;
-	}
-	
-	if((bind(listen_sock, (struct sockaddr *)&server_addres, sizeof(server_addres))) < 0){
-		printf("Could not bind socket\n");
-		return 1;
-	}
-	
-	int wait_size = 16;
-	
-	if(listen(listen_sock, wait_size) < 0){
-		printf("Could not open socket for listening\n");
-		return 1;
-	}
-	
+int CServer(int listen_sock){
 	struct sockaddr_in client_address;
 	int client_address_len = 0;
 	
-	while(true){
-		int sock;
- 
-		
+	int sock;
 		if((sock = accept(listen_sock, (struct sockaddr *)&client_address, &client_address_len)) < 0){
 			printf("Could not open a socket to accept data\n");
 			return 1;
@@ -64,25 +30,86 @@ int main(void)
 			printf("Recived: '%s'\n", buffer);
 			
 			send(sock, buffer, len, 0);
-			
-			
-			
 		}
-		
-		
 		
 		close(sock);
-		int i;
+	return 0;
+}
+
+int CClient(char ipAdres[1024]){
+	const int server_port = 25566;
+	struct sockaddr_in server_address;
+	int s;
+
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(server_port);
+	inet_aton("150.254.79.213", &server_address.sin_addr.s_addr);
 	
-		for(i = 0; i < maxlen; i++){
-			buffer[i] = '\0';
-		}
+	int sock;
+	if((sock = socket(PF_INET, SOCK_STREAM, 0 )) < 0){
+		printf("Could not create socket\n");
+		return 1;
 	}
 	
+	if(connect(sock, (struct sockaddr*)&server_address, sizeof(server_address)) < 0){
+		printf("Could not connect to server\n");
+		return 1;
+	}
 	
+	const char* data_to_send = "Test message";
+	send(sock, data_to_send, strlen(data_to_send), 0);
 	
-	close(listen_sock);
+	int n = 0;
+	int len = 0, maxlen = 100;
+	char buffer[maxlen];
+	char* pbuffer = buffer;
+	
+	while((n = recv(sock, pbuffer, maxlen, 0)) > 0){
+		pbuffer += n;
+		maxlen -=n;
+		len +=n;
+		
+		buffer[len] = '\0';
+		printf("recived: '%s'\n", buffer);
+	}
+	
+	close(sock);
+	return 0;
+}
+
+int main(void)
+{
+	int SERVER_PORT = 25565;
+	
+	struct sockaddr_in server_addres;
+	memset(&server_addres, 0, sizeof(server_addres));
+	server_addres.sin_family = AF_INET;
+	
+	server_addres.sin_port = htons(SERVER_PORT);
+	
+	server_addres.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	int listen_sock;
+	if((listen_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0){
+		printf("Could not create listen socket\n");
+		return 1;
+	}
+	
+	if((bind(listen_sock, (struct sockaddr *)&server_addres, sizeof(server_addres))) < 0){
+		printf("Could not bind socket\n");
+		return 1;
+	}
+	
+	int wait_size = 16;
+	
+	if(listen(listen_sock, wait_size) < 0){
+		printf("Could not open socket for listening\n");
+		return 1;
+	}
 	
 
+
+	
+	close(listen_sock);
 	return 0;
 }
